9724370f6d79f0ea5641e0283f76d315a1ec49ea
leol <ducleoliao@gmail.com>
2017-10-16 22:16:52 -0700

    Added Google Map Api

---
+++ frontend/components/map.jsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+//
+// class Map extends React.Componet {
+//   constructor(props){
+//       super(props);
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <h1>This is map </h1>
+//       </div>
+//     ); // return
+//   } // render
+// } // end class
+//
+// export default Map;
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import Map from './map'
 
 const Root = () => (
   <div>
@@ -7,3 +8,5 @@ const Root = () => (
 );
 
 export default Root;
+
+// <Map />
