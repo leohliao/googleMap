2d842fd8f5622b06ff81478d4020127c7c403e3c
leol <ducleoliao@gmail.com>
2017-10-17 00:10:05 -0700

    Added Searchbox and completion in searching the area

--- frontend/components/map.jsx
+++ frontend/components/map.jsx
@@ -18,8 +18,61 @@ class Map extends React.Component {
       center: {lat: 37.775, lng: -122.435},
       zoom: 13
     }) // const map
+
     const searchBox = new google.maps.places.SearchBox(input);
 
+    map.addListener('bounds_changed', function() {
+         searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+
+      // Clear out the old markers.
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    }); // end SearchBox Listener
 
   } // end componentDidMount
 
